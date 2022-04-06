a03a460add00039a5db53d5c57610971358fb7c0
Alex Hotca <ahotca2172@stu.d214.org>
2022-04-06 14:21:53 -0500

    added ViewController2 base

--- GitHubPractice2/ViewController2.swift
+++ GitHubPractice2/ViewController2.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class ViewController2: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var animalName: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        nameLabel.text = animalName
+        imageView.image = UIImage(named: animalName)
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
